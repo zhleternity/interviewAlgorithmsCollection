6c55f75576ff809db5037a6bd504a8c33ac07126
zhleternity <1021062123@qq.com>
2016-07-26 16:53:04 +0800

    Initial Commit

---
+++ InterviewAlgorithms/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  InterviewAlgorithms
+//
+//  Created by lingLong on 16/7/26.
+//  Copyright © 2016年 ling. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
